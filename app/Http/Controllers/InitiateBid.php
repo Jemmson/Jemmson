7c929883c6790ea07874f958b8d8809b4bc561ed
Shawn Pike <pike.shawn@gmail.com>
2017-07-05 19:30:20 -0700

    added a few remarks for the initiate bid controller

--- app/Http/Controllers/InitiateBid.php
+++ app/Http/Controllers/InitiateBid.php
@@ -15,5 +15,11 @@ class InitiateBid extends Controller
     public function send(Request $request)
     {
         dd($request);
+
+        // send a passwordless link if the email is not in the system
+        // this link will then redirect them to the bid page
+
+        // send a notification along with the passwordless link if the customer or sub is in the system
+
     }
 }
