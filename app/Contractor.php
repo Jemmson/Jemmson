940575652852ea8aaf3161c512b0405a3e2c3cf6
Daven <davene1919@gmail.com>
2017-07-29 12:48:48 -0700

    test

--- app/Contractor.php
+++ app/Contractor.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contractor extends Model
 {
-    //
+    // no empty comments
     public function user()
     {
         return $this->belongsTo(User::class);
