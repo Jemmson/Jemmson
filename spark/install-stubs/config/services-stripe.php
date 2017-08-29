bfea34f8478348ae8d15f93472543d5c07742b2e
Shawn Pike <pike.shawn@gmail.com>
2017-08-29 06:26:53 -0700

    added and tested that the test setup of stripe works in creating a test customer

--- spark/install-stubs/config/services-stripe.php
+++ spark/install-stubs/config/services-stripe.php
@@ -31,8 +31,8 @@ return [
 
     'stripe' => [
         'model'  => App\User::class,
-        'key'    => env('pk_test_9039SbKsp7OjAPg3pkSpbmTy'),
-        'secret' => env('sk_test_ebg7SjOI3rsZkeV5SZsUkOon'),
+        'key'    => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
     ],
 
 ];
