7899e607f2461d6688c300d0a78cdee48f451b22
Daven <davene1919@gmail.com>
2017-09-23 17:55:58 -0700

    id's for relationships need to be integer and unsigned

--- database/migrations/2017_07_09_195321_create_contractors_table.php
+++ database/migrations/2017_07_09_195321_create_contractors_table.php
@@ -16,7 +16,7 @@ class CreateContractorsTable extends Migration
         // this table keeps track of all of the users that are contractors
         Schema::create('contractors', function (Blueprint $table) {
             $table->increments('id');
-            $table->bigInteger('user_id')->unique();
+            $table->integer('user_id')->unsigned(); // NOTICE: deleted unique since it will not work with the many relationship
             $table->string('email_method_of_contact')->nullable();
             $table->string('address_line_1')->nullable();
             $table->string('address_line_2')->nullable();
