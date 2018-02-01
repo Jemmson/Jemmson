ef52d14a359e200b3dbacc5a4b705d38314e617d
Daven <Daven-@users.noreply.github.com>
2018-02-01 12:10:56 -0700

    new notification

---
+++ app/Notifications/CustomerSentStripePayment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+use App\User;
+
+class CustomerSentStripePayment extends Notification
+{
+    use Queueable;
+    protected $customer, $self;
+    /**
+     * Create a new notification instance.
+     *
+     * @param User $customer
+     * @param User $self
+     */
+    public function __construct(User $customer, User $self)
+    {
+        $this->customer = $customer;
+        $this->self = $self;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line($this->customer->name . ' has sent you a payment through stripe .')
+                    ->action('View Payment', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
