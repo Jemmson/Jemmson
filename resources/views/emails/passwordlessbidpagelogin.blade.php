28b55c4d365c280a37e165ae8174a19312528ee7
Daven <Daven-@users.noreply.github.com>
2018-02-03 09:57:33 -0700

    fix link

--- resources/views/emails/passwordlessbidpagelogin.blade.php
+++ resources/views/emails/passwordlessbidpagelogin.blade.php
@@ -1,4 +1,4 @@
 <h1>Hello,</h1>
 <p>{{ $contractor }} has initated a bid.</p>
 <p>Job Name: {{ $job_name }}</p>
-<label for="">View the bid here: {{ url('/login/' . $link . '/' . $job_id) }}</label>
+<label for="">View the bid here: {{ url('/login/customer/'. $job_id. '/' . $link) }}</label>
