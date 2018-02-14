935abf2b4c3650f44b8cc996e75de6b460e061b8
Daven <Daven-@users.noreply.github.com>
2018-02-14 12:11:30 -0700

    return only relevant fields

--- app/Http/Controllers/BidListController.php
+++ app/Http/Controllers/BidListController.php
@@ -28,8 +28,7 @@ class BidListController extends Controller
                 $query->with(
                 [
                   'jobTask' => function ($q) {
-                    // TODO: need to only return need to know columns, returns all data right now
-                    //$q->select('job_task.id', 'job_task.contractor_id', 'job_task.status', 'job_task.cust_final_price', 'job_task.start_date');
+                      $q->select('job_task.task_id', 'job_task.id', 'job_task.contractor_id', 'job_task.status', 'job_task.cust_final_price', 'job_task.start_date');
                   }
                 ]);
               }
