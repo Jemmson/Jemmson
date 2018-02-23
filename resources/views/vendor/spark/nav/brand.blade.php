d38ed115ff771d4968e6b8bfa3e79a8cffcda0b0
Daven <Daven-@users.noreply.github.com>
2018-02-23 09:20:35 -0700

    show contractor logo in top right

--- resources/views/vendor/spark/nav/brand.blade.php
+++ resources/views/vendor/spark/nav/brand.blade.php
@@ -1,3 +1,16 @@
 <a class="navbar-brand" href="/home">
-    <img src="/img/mono-logo.png" style="height: 32px;">
+    <img id="user_logo" src="" style="height: 32px;">
 </a>
+<script>
+    let img = document.getElementById('user_logo');
+    console.log(img);
+    if (Spark.state.user !== undefined && Spark.state.user !== null) {
+        if (Spark.state.user.logo_url !== null) {
+            img.src = Spark.state.user.logo_url;
+        } else {
+             img.src = "/img/mono-logo.png";
+        }
+    } else {
+        img.src = "/img/mono-logo.png";
+    }
+</script>
