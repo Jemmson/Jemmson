51f5baa56f6e03159e7199127067964460424879
Daven <Daven-@users.noreply.github.com>
2018-03-30 09:42:41 -0700

    no longer using blade for front-end stuff

--- resources/views/tasks/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('spark::layouts.app')
-
-@section('content')
-    <!-- /assumed they have tasks? should we need to check if they are a subcontractor? -->
-    <subcontractor-tasks :user="user" :bid-tasks="{{ $tasks }}">
-    </subcontractor-tasks>
-@endsection
