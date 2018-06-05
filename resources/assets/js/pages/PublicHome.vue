5d20e93035065544c09078712cc64afb66d783dc
Shawn Pike <pike.shawn@gmail.com>
2018-06-04 20:50:35 -0700

    updating home page and navbar

--- resources/assets/js/pages/PublicHome.vue
+++ resources/assets/js/pages/PublicHome.vue
@@ -1,84 +1,27 @@
 <template>
-    <div>
-        <div style="text-align: center; margin-bottom: 1rem;">
-            <div class="btn-group">
-                <button class="login btn btn-lg btn-primary" v-on:click="route('login')">LOGIN</button>
-                <button class="register btn btn-lg btn-primary" v-on:click="route('register')">REGISTER</button>
-            </div>
+    <div class="wrapper">
+        <div class="title"><h1 class="text-center">Jemsub</h1></div>
+        <div class="section main">
+            <h2>Subcontract Work</h2>
+            <p>How often at a job do you run into work that you do not do or work you dont have time for?
+                How often do you simply lose income because you either call someone to handle the job for you or
+                just tell the customer to find someone else? Now with this app you can easily create a task and then
+                subcontract that task, mark up the task, and then easily get paid for the work.</p>
         </div>
-        <div class="container">
-
-            <!-- Heading Row -->
-            <div class="row">
-                <div class="col-md-8">
-                    <img class="img-responsive img-rounded" src="/img/TreeGrove.jpg" alt="">
-                </div>
-                <!-- /.col-md-8 -->
-                <div class="col-md-4">
-                    <h1 class="text-center">JEMSUB</h1>
-                    <p>Welcome to your place where you can SUBCONTRACT work you dont have time for. A place where you
-                        can INVOICE your customers and get paid using Stripe or with cash.</p>
-                    <!--<a class="btn btn-primary btn-lg" href="#">Call to Action!</a>-->
-                </div>
-                <!-- /.col-md-4 -->
-            </div>
-            <!-- /.row -->
-
-            <hr>
-
-            <!-- Call to Action Well -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="well text-center">
-                        SUBCONTRACT, INVOICE, GET PAID, EASILY!
-                    </div>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-
-            <!-- Content Row -->
-            <div class="row">
-                <div class="col-md-4">
-                    <h2>Subcontract Work</h2>
-                    <p>How often at a job do you run into work that you do not do or work you dont have time for?
-                    How often do you simply lose income because you either call someone to handle the job for you or
-                    just tell the customer to find someone else? Now with this app you can easily create a task and then
-                     subcontract that task, mark up the task, and then easily get paid for the work.</p>
-                    <!--<a class="btn btn-default" href="#">More Info</a>-->
-                </div>
-                <!-- /.col-md-4 -->
-                <div class="col-md-4">
-                    <h2>Invoice Customers</h2>
-                    <p>You can create an invoice at the beginning of a job and then use that as a commmunication tool
-                    throughout the jobs life cycle. You simply initiate a bid for all jobs. Then construct that bid,
-                        subcontract any of those tasks, and then submit the bid to the customer. The customer then has
-                    the ability to approve the bid. Once the bid has been approve then the work can be performed. Once the
-                    work has been performed the customer can now approve the finish work and pay using Stripe or cash.</p>
-                    <!--<a class="btn btn-default" href="#">More Info</a>-->
-                </div>
-                <!-- /.col-md-4 -->
-                <div class="col-md-4">
-                    <h2>Get Paid Easily </h2>
-                    <p>How much money is wasted chasing down a check? How much of a pain is it to have to write a check
-                        to a subcontractor after a job has been finished? With Stripe this will happen easily.
-                     The customer pays the invoice and then the money is automatically split between the contractor
-                        and the sub.</p>
-                    <!--<a class="btn btn-default" href="#">More Info</a>-->
-                </div>
-                <!-- /.col-md-4 -->
-            </div>
-            <!-- /.row -->
-
-            <!-- Footer -->
-            <footer>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <p>Copyright &copy; Your Website 2018</p>
-                    </div>
-                </div>
-            </footer>
-
+        <div class="section pricing">
+            <h2 style="color:black">Invoice Customers</h2>
+            <p style="color:black">You can create an invoice at the beginning of a job and then use that as a commmunication tool
+                throughout the jobs life cycle. You simply initiate a bid for all jobs. Then construct that bid,
+                subcontract any of those tasks, and then submit the bid to the customer. The customer then has
+                the ability to approve the bid. Once the bid has been approve then the work can be performed. Once the
+                work has been performed the customer can now approve the finish work and pay using Stripe or cash.</p>
+        </div>
+        <div class="section features">
+            <h2>Get Paid Easily</h2>
+            <p>How much money is wasted chasing down a check? How much of a pain is it to have to write a check
+                to a subcontractor after a job has been finished? With Stripe this will happen easily.
+                The customer pays the invoice and then the money is automatically split between the contractor
+                and the sub.</p>
         </div>
     </div>
 </template>
@@ -97,3 +40,67 @@
     }
   }
 </script>
+
+<style scoped>
+
+    .wrapper {
+        display: grid;
+        width: 100%;
+    }
+
+    .section {
+        height: 630px;
+        width: 100%;
+    }
+
+    .title {
+        background-color: white;
+    }
+
+    .main {
+        background-color: #203C4E;
+    }
+
+    .pricing {
+        background-color: white;
+    }
+
+    .features {
+        background-color: #378372;
+    }
+
+    h2 {
+        color: rgb(255, 255, 255);
+        padding: 3rem;
+        font-family: "Open Sans", sans-serif;
+        font-size: 40px;
+        font-weight: 300;
+        height: 88px;
+        margin-left: 1rem;
+        margin-top: 1rem;
+    }
+
+    p {
+        color: rgb(255, 255, 255);
+        padding-left: 4rem;
+        padding-right: 4rem;
+        padding-top: 3rem;
+        font-family: "Open Sans", sans-serif;
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 27px;
+        margin-top: 15px;
+        /*width: 345px;*/
+    }
+
+    @media (min-width: 300px) {
+
+    }
+
+    @media (min-width: 1100px) {
+        h2 {
+            display: none;
+        }
+    }
+
+</style>
--- resources/views/vendor/spark/nav/contractor-left.blade.php
+++ resources/views/vendor/spark/nav/contractor-left.blade.php
@@ -1,16 +1,6 @@
 <!-- Left Side Of Navbar -->
-<li>
-    <a href="/#/home">Home</a>
-</li>
-<li>
-    <a href="/#/initiate-bid">Initiate Bid</a>
-</li>
-<li>
-    <a href="/#/bids">Bids</a>
-</li>
-<li>
-    <a href="/#/tasks">Tasks</a>
-</li>
-<li>
-    <a href="/#/invoices">Invoices</a>
-</li>
+<span><a href="/#/home">Home</a></span>
+<span><a href="/#/initiate-bid">Initiate Bid</a></span>
+<span><a href="/#/bids">Bids</a></span>
+<span><a href="/#/tasks">Tasks</a></span>
+<span><a href="/#/invoices">Invoices</a></span>
