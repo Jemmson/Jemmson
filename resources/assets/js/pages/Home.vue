20d8745f19bbaec38690f737c61e5a916e96084d
Shawn Pike <pike.shawn@gmail.com>
2018-06-17 16:44:39 -0700

    wiring in the home page for a summary

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -9,39 +9,71 @@
         <h3 class="home-sub-heading">Bids</h3>
         <hr>
         <div class="home-summary-data">
+            <h4>Initiated</h4>
+            <h5 class="needsApproval">{{ bidData(bids, 'bid.initiated') }}</h5>
+        </div>
+        <div class="home-summary-data">
+            <h4>In Progress</h4>
+            <h5 class="inProcess">{{ bidData(bids, 'bid.in_progress') }}</h5>
+        </div>
+        <div class="home-summary-data">
             <h4>Needs Approval</h4>
-            <h5>5</h5>
+            <h5 class="inProcess">{{ bidData(bids, 'bid.sent') }}</h5>
         </div>
         <div class="home-summary-data">
-            <h4>In Process</h4>
-            <h5>3</h5>
+            <h4>Declined</h4>
+            <h5 class="inProcess">{{ bidData(bids, 'bid.declined') }}</h5>
         </div>
         <div class="home-summary-data">
-            <h4>Waiting to be finished</h4>
-            <h5>4</h5>
+            <h4>Job Approved (Waiting to be Finished)</h4>
+            <h5 class="waitingToBeFinished">{{ bidData(bids, 'job.approved') }}</h5>
         </div>
         <div class="home-summary-data">
             <h4>Number of Finished Jobs</h4>
-            <h5>4</h5>
+            <h5 class="finishedJobs">{{ bidData(bids, 'job.completed') }}</h5>
         </div>
         <div v-if="(user.usertype === 'contractor') && user.contractor !== null">
             <h3 class="home-sub-heading home-sub-heading-tasks">Tasks</h3>
             <hr>
             <div class="home-summary-data">
-                <h4>Needs To be Bid on</h4>
-                <h5>5</h5>
+                <h4>Initiated</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.initiated') }}</h5>
             </div>
             <div class="home-summary-data">
-                <h4>To Be Finished</h4>
-                <h5>3</h5>
+                <h4>Sent</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.bid_sent') }}</h5>
             </div>
             <div class="home-summary-data">
-                <h4>Waiting on Customer Payment</h4>
-                <h5>4</h5>
+                <h4>Accepted</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.accepted') }}</h5>
             </div>
             <div class="home-summary-data">
-                <h4>Paid</h4>
-                <h5>4</h5>
+                <h4>Finished</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.finished_by_sub') }}</h5>
+            </div>
+            <div class="home-summary-data">
+                <h4>Approved by General</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.approved_by_general') }}</h5>
+            </div>
+            <div class="home-summary-data">
+                <h4>Sent to Customer for Approval</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.finished_by_general') }}</h5>
+            </div>
+            <div class="home-summary-data">
+                <h4>Approved by Customer</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.approved_by_customer') }}</h5>
+            </div>
+            <div class="home-summary-data">
+                <h4>Customer Sent Payment</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.customer_sent_payment') }}</h5>
+            </div>
+            <div class="home-summary-data">
+                <h4>Reopened Tasks</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.reopened') }}</h5>
+            </div>
+            <div class="home-summary-data">
+                <h4>Denied Tasks</h4>
+                <h5 class="needsToBeBidOn">{{ taskData(tasks, 'bid_task.denied') }}</h5>
             </div>
         </div>
         <div class="summary-footer"></div>
@@ -55,7 +87,10 @@
       user: Object
     },
     data () {
-      return {}
+      return {
+        bids: '',
+        tasks: ''
+      }
     },
     computed: {},
     methods: {
@@ -68,9 +103,38 @@
         } else {
           this.$router.push (value)
         }
-      }
+      },
+      bidData (bids, message) {
+        let count = 0;
+        for (let i = 0; i < bids.length; i++) {
+          if (bids[i].status === message) {
+            count++;
+          }
+        }
+        return count;
+      },
+      taskData (tasks, message) {
+        let count = 0;
+        for (let i = 0; i < tasks.length; i++) {
+          if (tasks[i].job_task.status === message) {
+            count++;
+          }
+        }
+        return count;
+      },
     },
     mounted: function () {
+      console.log ('getBids');
+      axios.post ('/jobs').then ((response) => {
+        this.bids = response.data;
+        this.sBids = this.bids;
+        console.log (this.bids)
+      });
+      console.log('getTasks');
+      axios.post('/bid/tasks').then((response) => {
+        this.tasks = response.data;
+        this.sTasks = this.tasks;
+      });
     }
   }
 </script>
