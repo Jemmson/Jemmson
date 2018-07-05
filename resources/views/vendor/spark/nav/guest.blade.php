7b5a6023f83935c4bf8f50ed04a98f5dec525fc4
Shawn Pike <pike.shawn@gmail.com>
2018-07-05 14:53:22 -0700

    some css adjustments

--- resources/views/vendor/spark/nav/guest.blade.php
+++ resources/views/vendor/spark/nav/guest.blade.php
@@ -1,10 +1,16 @@
-<div class="jemmson-navbar">
+<div class="jemmson-navbar
+            flex
+            justify-between
+            items-center">
     <span><a href="/#/benefits">Benefits</a></span>
     <span><a href="/#/howto">How To</a></span>
     <span><a href="/#/demo">Demo</a></span>
 </div>
 
-<div class="jemmson-footer flex">
+<div class="jemmson-footer
+            flex
+            justify-between
+            items-center">
     <span><a href="/login">Login</a></span>
     <span><a href="/register">Register</a></span>
 </div>
