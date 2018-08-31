5ea25dee87d27ab8c22b30d1b641b79188bf3584
Daven <Daven-@users.noreply.github.com>
2018-08-31 10:30:40 -0700

    require bootstrap in test specs when needed

--- tests/JS/jobs.spec.js
+++ tests/JS/jobs.spec.js
@@ -1,60 +1,60 @@
-// import {
-//     mount,
-//     createLocalVue
-// } from 'vue-test-utils'
-// import VuePaginate from 'vue-paginate'
+import {
+    mount,
+    createLocalVue
+} from 'vue-test-utils'
+import VuePaginate from 'vue-paginate'
+
+require('./bootstrap');
+
+const localVue = createLocalVue()
+localVue.use(VuePaginate)
 
 
-// const localVue = createLocalVue()
-// localVue.use(VuePaginate)
+import Jobs from '../../resources/assets/js/pages/Jobs.vue'
 
-// // global.Bus = localVue;
-
-// import Jobs from '../../resources/assets/js/pages/Jobs.vue'
-
-// const $on = {
-//     bidUpdated: () => {
-//         console.log('get bids');
+const $on = {
+    bidUpdated: () => {
+        console.log('get bids');
         
-//     }
-// }
+    }
+}
 
-// describe('Jobs', () => {
-//   const wrapper = mount (Jobs, {
-//     localVue,
-//     mocks: {
-//         $on
-//     },
-//     propsData: {
-//       user: {
-//         usertype: 'contractor',
-//         contractor: {
-//           company_name: 'KPS Pools'
-//         }
-//       }
-//     },
-//     data () {
-//       return {
-//         bids: [],
-//         sBids: [
-//           {
-//             bid_price: 245,
-//             customer: {
-//               name: 'Laurel Ailie'
-//             },
-//             job_name: 'Clear up Green Pool',
-//             status: 'bid.sent'
-//           }
-//         ],
-//         showBid: false,
-//         bidIndex: 0,
-//         searchTerm: '',
-//         paginate: ['sBids']
-//       }
-//     }
-//   });
+describe('Jobs', () => {
+  const wrapper = mount (Jobs, {
+    localVue,
+    mocks: {
+        $on
+    },
+    propsData: {
+      user: {
+        usertype: 'contractor',
+        contractor: {
+          company_name: 'KPS Pools'
+        }
+      }
+    },
+    data () {
+      return {
+        bids: [],
+        sBids: [
+          {
+            bid_price: 245,
+            customer: {
+              name: 'Laurel Ailie'
+            },
+            job_name: 'Clear up Green Pool',
+            status: 'bid.sent'
+          }
+        ],
+        showBid: false,
+        bidIndex: 0,
+        searchTerm: '',
+        paginate: ['sBids']
+      }
+    }
+  });
 
-//   it ('should ', function () {
+  it ('should ', function () {
 
-//   });
-// })
+  });
+})
