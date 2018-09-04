49f8956fe2dbb0b8896c69d709dd8d4d166c121e
Daven <Daven-@users.noreply.github.com>
2018-09-04 14:47:03 -0700

    couple tests

--- tests/JS/jobs.spec.js
+++ tests/JS/jobs.spec.js
@@ -20,96 +20,109 @@ const $on = {
 }
 
 describe('Jobs', () => {
-  const wrapper = mount(Jobs, {
-    localVue,
-    mocks: {
-        $on
-    },
-    propsData: {
-      user: {
-        usertype: 'contractor',
-        contractor: {
-          company_name: 'KPS Pools'
+    const wrapper = mount(Jobs, {
+        localVue,
+        mocks: {
+            $on
+        },
+        propsData: {
+            user: {
+                usertype: 'contractor',
+                contractor: {
+                    company_name: 'KPS Pools'
+                }
+            }
+        },
+        data() {
+            return {
+                bids: [],
+                sBids: [{
+                    bid_price: 245,
+                    customer: {
+                        name: 'Laurel Ailie'
+                    },
+                    job_name: 'Clear up Green Pool',
+                    status: 'bid.sent'
+                }, {
+                    bid_price: 245,
+                    customer: {
+                        name: 'Jane Doe'
+                    },
+                    job_name: 'Fix Sink',
+                    status: 'bid.sent'
+                }, {
+                    bid_price: 245,
+                    customer: {
+                        name: 'Jane Doe'
+                    },
+                    job_name: 'Fix Sink',
+                    status: 'bid.sent'
+                }, {
+                    bid_price: 245,
+                    customer: {
+                        name: 'Jane Doe'
+                    },
+                    job_name: 'Fix Sink',
+                    status: 'bid.sent'
+                }, {
+                    bid_price: 245,
+                    customer: {
+                        name: 'Jane Doe'
+                    },
+                    job_name: 'Fix Sink',
+                    status: 'bid.sent'
+                }, {
+                    bid_price: 245,
+                    customer: {
+                        name: 'Jane Doe'
+                    },
+                    job_name: 'Fix Sink',
+                    status: 'bid.sent'
+                }, {
+                    bid_price: 245,
+                    customer: {
+                        name: 'Jane Doe'
+                    },
+                    job_name: 'Fix Sink',
+                    status: 'bid.sent'
+                }],
+                showBid: false,
+                bidIndex: 0,
+                searchTerm: '',
+                paginate: ['sBids']
+            }
         }
-      }
-    },
-    data () {
-      return {
-        bids: [],
-        sBids: [
-          {
-            bid_price: 245,
-            customer: {
-              name: 'Laurel Ailie'
-            },
-            job_name: 'Clear up Green Pool',
-            status: 'bid.sent'
-          }, {
-              bid_price: 245,
-              customer: {
-                  name: 'Jane Doe'
-              },
-              job_name: 'Fix Sink',
-              status: 'bid.sent'
-          }, {
-              bid_price: 245,
-              customer: {
-                  name: 'Jane Doe'
-              },
-              job_name: 'Fix Sink',
-              status: 'bid.sent'
-          }, {
-              bid_price: 245,
-              customer: {
-                  name: 'Jane Doe'
-              },
-              job_name: 'Fix Sink',
-              status: 'bid.sent'
-          }, {
-              bid_price: 245,
-              customer: {
-                  name: 'Jane Doe'
-              },
-              job_name: 'Fix Sink',
-              status: 'bid.sent'
-          }, {
-              bid_price: 245,
-              customer: {
-                  name: 'Jane Doe'
-              },
-              job_name: 'Fix Sink',
-              status: 'bid.sent'
-          }, {
-              bid_price: 245,
-              customer: {
-                  name: 'Jane Doe'
-              },
-              job_name: 'Fix Sink',
-              status: 'bid.sent'
-          }
-        ],
-        showBid: false,
-        bidIndex: 0,
-        searchTerm: '',
-        paginate: ['sBids']
-      }
-    }
-  });
+    });
 
-  it('Should contain the name Fix Sink', () => {
-    expect(wrapper.text()).toContain('Fix Sink');
-  });
+    it('Should contain the name Fix Sink', () => {
+        expect(wrapper.text()).toContain('Fix Sink');
+    });
 
-  it('Should have rendered 6 job items', () => {
-      expect(wrapper.findAll('section')).toHaveLength(6);
-  });
-  
-  it('Should have 2 paginate links', () => {
-      expect(wrapper.html()).toContain('<a>1</a>');
-      expect(wrapper.html()).toContain('<a>2</a>');
-  });
+    it('Should have rendered 6 job items', () => {
+        expect(wrapper.findAll('section')).toHaveLength(6);
+    });
 
-  it('Should not have 3th paginate link', () => {
-  });
+    it('Should have 2 paginate links', () => {
+        expect(wrapper.html()).toContain('<a>1</a>');
+        expect(wrapper.html()).toContain('<a>2</a>');
+    });
+
+    it('Clicking the next arrow should show the next list of jobs', () => {
+        wrapper.find('.right-arrow > a').trigger('click');
+        expect(wrapper.findAll('section')).toHaveLength(1);
+    });
+
+    it('Clicking the previous arrow should show the previous list of jobs', () => {
+        wrapper.find('.left-arrow > a').trigger('click');
+        expect(wrapper.findAll('section')).toHaveLength(6);
+    });
+
+    it('Search bar should change', () => {
+        const input = wrapper.find('input');
+        input.setValue('Clear up Green Pool');
+        // input.trigger('keyup');
+        expect(wrapper.vm.searchTerm).toBe('Clear up Green Pool');
+        // expect(wrapper.findAll('section')).toHaveLength(1);
+    });
 
 });
