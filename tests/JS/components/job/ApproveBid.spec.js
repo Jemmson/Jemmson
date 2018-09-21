2cb15c996de9fe364c82799e3373fd771584dbac
Daven <Daven-@users.noreply.github.com>
2018-09-21 13:32:31 -0700

    some test stubs

--- tests/JS/components/job/ApproveBid.spec.js
+++ tests/JS/components/job/ApproveBid.spec.js
@@ -28,7 +28,7 @@ describe('Completed Tasks', () => {
                 job_name: 'Pool Job',
                 agreed_start_date: '2018-08-01 10:58:37',
                 status: 'bid.initiated',
-                contractor_id: 1,//
+                contractor_id: 1,
                 bid_price: 99.00,
                 declined_message: null,
                 location: null
---
+++ tests/JS/components/job/GeneralContractorBidActions.spec.js
@@ -0,0 +1,36 @@
+import {
+    shallowMount
+} from "@vue/test-utils";
+import GeneralContractorBidActions from '../../../../resources/assets/js/components/job/GeneralContractorBidActions';
+
+require('../../bootstrap');
+
+describe('Completed Tasks', () => {
+    const wrapper = shallowMount(GeneralContractorBidActions, {
+        methods: {
+        },
+        stubs: [
+            'modal'
+        ],
+        propsData: {
+            bid: {
+                job_name: 'Pool Job',
+                agreed_start_date: '2018-08-01 10:58:37',
+                status: 'bid.initiated',
+                contractor_id: 1, //
+                bid_price: 99.00,
+                declined_message: null,
+                location: null,
+                job_tasks: [
+                    {},
+                    {}
+                ]
+            }
+        }
+    });
+
+    it('Should render itself', () => {
+        expect(wrapper.isEmpty()).toBe(false);
+    });
+
+});
