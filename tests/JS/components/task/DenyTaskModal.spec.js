472f1c5ba85696b9f1be75520b6f7d9223200443
Daven <Daven-@users.noreply.github.com>
2018-10-01 11:28:32 -0700

    deny task modal tests

--- tests/JS/components/task/DenyTaskModal.spec.js
+++ tests/JS/components/task/DenyTaskModal.spec.js
@@ -1,11 +1,14 @@
 import {
     shallowMount
 } from "@vue/test-utils";
+import sinon from 'sinon';
 import DenyTaskModal from '../../../../resources/assets/js/components/task/DenyTaskModal';
 
 require('../../bootstrap');
 
+
 describe('Deny Task Modal', () => {
+    const denyTask = sinon.spy();
     const wrapper = shallowMount(DenyTaskModal, {
         propsData: {
             jobTask: {
@@ -13,10 +16,21 @@ describe('Deny Task Modal', () => {
                     name: "Clean Pool"
                 }
             }
+        },
+        methods: {
+            denyTask
         }
     });
 
-    it('Should be true', () => {
-        expect(true).toBe(false);
+    it('Should render itself', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('Should call denyTask when deny button is clicked', () => {
+        const denyTaskBtn = wrapper.find({
+            ref: 'denyTaskBtn'
+        })
+        denyTaskBtn.trigger('click');
+        expect(denyTask.calledOnce).toBe(true);
     });
 });
