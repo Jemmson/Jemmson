cbf86b1031a97aac45c05b8a2db73769b508964f
Daven <Daven-@users.noreply.github.com>
2018-10-01 09:31:50 -0700

    page stubs

---
+++ tests/JS/Invoice.spec.js
@@ -0,0 +1,104 @@
+import {
+  mount,
+  shallowMount,
+  createLocalVue
+}
+  from '@vue/test-utils'
+import sinon from 'sinon'
+
+const $route = {
+  path: '/#/bid/1',
+  query: {
+    success: 1,
+    error: 0
+  },
+  params: {
+    id: 1
+  }
+}
+
+require('./bootstrap')
+
+import Invoice from '../../resources/assets/js/pages/Invoice.vue'
+
+describe('Invoice', () => {
+  const wrapper = shallowMount(Invoice, {
+    mocks: {
+      $route
+    },
+    stubs: [
+      'bid-details',
+      'approve-bid',
+      'completed-tasks',
+      'bid-tasks',
+      'bid-add-task',
+      'stripe',
+      'card',
+      'general-contractor-bid-actions'
+    ],
+    slots: {
+      'card-footer': ['<general-contractor-bid-actions />']
+    },
+    methods: {
+      getBid(id) {
+        switch (id) {
+          case 1:
+            this.bid = {
+              id: 1,
+              status: 'bid.in_progress',
+              job_tasks: [{
+                id: 1,
+              }]
+            };
+            break;
+          case 2: 
+            Vue.toasted.error('Error');
+            break;
+          default:
+            break;
+        }
+      }
+    },
+    propsData: {
+      user: global.User.user,
+    }
+  })
+
+  it('Should render the card component', () => {
+    expect(wrapper.find('card-stub').exists()).toBe(true)
+  })
+
+  it('Should render the bid-details component', () => {
+    expect(wrapper.find('bid-details-stub').exists()).toBe(true)
+  })
+
+  it('Should not render the approve-bid component - contractor', () => {
+    expect(wrapper.find('approve-bid-stub').exists()).toBe(false)
+  })
+  
+  it('Should render the general-contractor-bid-actions component', () => {
+    expect(wrapper.find('general-contractor-bid-actions').exists()).toBe(false) // wtf
+  })
+
+  it('Should render the completed-tasks component', () => {
+    expect(wrapper.find('completed-tasks-stub').exists()).toBe(true)
+  })
+
+  it('Should render the bid-tasks component', () => {
+    expect(wrapper.find('bid-tasks-stub').exists()).toBe(true)
+  })
+
+  it('Should render the bid-add-task component', () => {
+    expect(wrapper.find('bid-add-task-stub').exists()).toBe(true)
+  })
+
+  it('Should render the stripe component', () => {
+    expect(wrapper.find('stripe-stub').exists()).toBe(true)
+  })
+
+  it('Should not render the bid-tasks component', () => {
+    
+    expect(wrapper.find('bid-tasks-stub').exists()).toBe(false)
+  })
+  
+})
