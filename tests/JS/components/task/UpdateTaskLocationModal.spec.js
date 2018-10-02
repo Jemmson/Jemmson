c3bc8532b848003a52609454a5aea71acbab7436
Daven <Daven-@users.noreply.github.com>
2018-10-02 11:10:09 -0700

    updatetask location test

--- tests/JS/components/task/UpdateTaskLocationModal.spec.js
+++ tests/JS/components/task/UpdateTaskLocationModal.spec.js
@@ -1,11 +1,13 @@
 import {
     shallowMount
 } from "@vue/test-utils";
+import sinon from 'sinon';
 import UpdateTaskLocationModal from '../../../../resources/assets/js/components/task/UpdateTaskLocationModal';
 
 require('../../bootstrap');
 
 describe('Update Task Location Modal', () => {
+    const update = sinon.spy();
     const wrapper = shallowMount(UpdateTaskLocationModal, {
         propsData: {
             jobTask: {
@@ -13,6 +15,7 @@ describe('Update Task Location Modal', () => {
             }
         },
         methods: {
+            update,
             initAutocomplete () {
                 return;
             }
@@ -21,5 +24,11 @@ describe('Update Task Location Modal', () => {
 
     it('Should render itself', () => {
         expect(wrapper.isEmpty()).toBe(false);
+    });
+
+    it('Should try and update task location', () => {
+        const submit = wrapper.find('[type=submit]');
+        submit.trigger('click');
+        expect(update.calledOnce).toBe(true);
     });
 });
