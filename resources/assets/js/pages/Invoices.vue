ce2df299997c7aa0eaf3057138abfe8d48a0a56f
Shawn Pike <pike.shawn@gmail.com>
2019-01-06 21:41:59 -0700

    expanded the click surface area of the invoices link

--- resources/assets/js/pages/Invoices.vue
+++ resources/assets/js/pages/Invoices.vue
@@ -1,19 +1,39 @@
 <template>
-    <div class="flex flex-col">
-        <search-bar>
-            <input type="text" placeholder="Search Invoices" v-model="searchTerm" @keyup="search">
-        </search-bar>
-        <paginate v-show="sInvoices.length > 0" ref="paginator" name="sInvoices" :list="sInvoices" :per="8"
-                  class="paginated">
-            <card v-for="invoice in paginated('sInvoices')" v-bind:key="invoice.id">
-                <div v-if="invoice.job_id !== undefined" class="self-center">
-                    <router-link :to="'/sub/invoice/' + invoice.id" class="w-full">{{invoice.task.name}}</router-link>
+    <div class="flex flex-col justify-between full-height">
+        <div>
+            <search-bar>
+                <input
+                        type="text"
+                        placeholder="Search Invoices"
+                        v-model="searchTerm"
+                        @keyup="search">
+            </search-bar>
+            <paginate v-show="sInvoices.length > 0"
+                      ref="paginator" name="sInvoices"
+                      :list="sInvoices"
+                      :per="8"
+                      class="paginated">
+                <div v-for="invoice in paginated('sInvoices')"
+                     v-bind:key="invoice.id"
+                     class="invoice-section"
+                >
+                    <div
+                            v-if="invoice.job_id !== undefined"
+                            class="sizing text-center w-full h-full"
+                            @click="goToInvoice('/sub/invoice/' + invoice.id)"
+                    >
+                        {{invoice.task.name}}
+                    </div>
+                    <div
+                            v-else
+                            @click="goToInvoice('/invoice/' + invoice.id)"
+                            class="sizing text-center w-full h-full"
+                    >
+                        {{invoice.job_name}}
+                    </div>
                 </div>
-                <div v-else class="self-center">
-                    <router-link :to="'/invoice/' + invoice.id" class="w-full">{{invoice.job_name}}</router-link>
-                </div>
-            </card>
-        </paginate>
+            </paginate>
+        </div>
         <div class="card p-5 card-body justify-center">
             <paginate-links for="sInvoices" :limit="2" :show-step-links="true">
             </paginate-links>
@@ -24,13 +44,15 @@
 
 <script>
 
-  import SearchBar from '../components/shared/SearchBar';
-  import Feedback from '../components/shared/Feedback';
+  import SearchBar from '../components/shared/SearchBar'
+  import Feedback from '../components/shared/Feedback'
+  import Card from '../components/shared/Card'
 
   export default {
     components: {
       SearchBar,
-      Feedback
+      Feedback,
+      Card
     },
     props: {
       user: Object,
@@ -47,6 +69,10 @@
     methods: {
       invoiceLink(id) {
         console.log('invoice id: ' + id)
+      },
+      goToInvoice(location) {
+        // debugger;
+        this.$router.push(location)
       },
       search() {
         console.log('searching: ' + this.searchTerm)
@@ -67,3 +93,21 @@
     }
   }
 </script>
+
+<style scoped>
+    .full-height {
+        height: 125vh;
+    }
+
+    .sizing {
+        font-size: 14pt;
+        padding: .75rem 0;
+    }
+
+    .invoice-section {
+        height: 3rem;
+        width: 100%;
+        background-color: white;
+        border-radius: 5px;
+    }
+</style>
