b720d3ff71469e7a5e59a00b3cbf3f216a20db94
Daven <Daven-@users.noreply.github.com>
2019-04-08 11:32:35 -0700

    reset style and import bootstrap 4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,7 @@
 
     <!-- CSS -->
     <link href="/css/sweetalert.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
 
