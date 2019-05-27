4534c448cb3a83f11f7adf47ef53131bfd8dfb1e
Daven <Daven-@users.noreply.github.com>
2019-05-27 12:24:19 -0700

    Update Invoice.vue

--- resources/assets/js/pages/Invoice.vue
+++ resources/assets/js/pages/Invoice.vue
@@ -1,96 +1,68 @@
 <template>
-    <div class="flex flex-col">
+  <div class="container">
+    <div v-if="invoice !== null" ref="invoice-details" class="row">
+      <div class="col-12 mb-4">
+        <h1 class="card-title">Details</h1>
         <card>
-            <h1 class="self-center">Invoice Page</h1>
+          <div class="row">
+            <div class="col-12 mb-3">
+              <span class="">Job Name:</span>
+              <span class="float-right">{{ invoice.job_name }}</span>
+            </div>
+            <div class="col-12 mb-3">
+              <span class="">Date Completed:</span>
+              <span class="float-right">{{ invoice.actual_end_date.split(' ')[0] }}</span>
+            </div>
+            <div class="col-12 mb-2">
+              <span class="">Total Price:</span>
+              <span class="float-right">${{ invoice.bid_price/100 }}</span>
+            </div>
+          </div>
         </card>
-        <div v-if="invoice !== null" ref="invoice-details">
-            <card>
-                <section class="flex flex-col">
-                    <h3 for="company_name" v-if="isContractor" class="mb-4">{{ user.contractor.company_name }}</h3>
-                    <h3 for="company_name" v-if="!isContractor" class="mb-4">{{ user.name }}</h3>
-                    <div class="flex justify-between">
+      </div>
 
-                        <div>
-                            <label for="job_name" class="label">
-                                Job Name:
-                            </label>
-                            <p>
-                                {{ invoice.job_name }}
-                            </p>
-                        </div>
-
-                        <div>
-                            <label class="label">
-                                Cash Payment Instructions
-                            </label>
-                            <p>
-                                {{ invoice.paid_with_cash_message }}
-                            </p>
-                        </div>
-
-                        <div>
-                            <label for="title" class="label">
-                                Date Completed:
-                            </label>
-                            <p>
-                                {{ invoice.updated_at }}
-                            </p>
-                        </div>
-                    </div>
-                    <address v-if="invoice.location !== null">
-                        <br> {{ invoice.location.address_line_1 }}
-                        <br> {{ invoice.location.city }}, {{ invoice.location.state }} {{ invoice.location.zip }}
-                    </address>
-                </section>
-            </card>
-
-            <card>
-                <div class="flex flex-col">
-                    <div class="flex justify-between">
-                        <div class="invoice-item invoice-header-item">Task Name</div>
-                        <div class="invoice-item invoice-header-item">QTY</div>
-                        <div class="invoice-item invoice-header-item">Task Price</div>
-                        <div class="invoice-item invoice-header-item" v-if="isContractor">Task Price (Sub)</div>
-                        <div class="invoice-item invoice-header-item" v-if="isContractor">Profit</div>
-                    </div>
-                    <div class="flex flex-col" v-if="invoice.job_tasks.length > 0">
-                        <div class="flex justify-between mb-2 mt-2" v-for="task in invoice.job_tasks" :key="task.id">
-                            <div class="invoice-item">{{ task.task.name }}</div>
-                            <div class="invoice-item">{{ task.qty }}</div>
-                            <div class="invoice-item">${{ task.cust_final_price }}</div>
-                            <div class="invoice-item" v-if="isContractor">${{ task.sub_final_price }}</div>
-                            <div class="invoice-item" v-if="isContractor">${{ task.cust_final_price -
+      <div class="col-12">
+        <card>
+          <div class="row">
+            <div class="col-12">
+              <div class="">Task Name</div>
+              <div class="">QTY</div>
+              <div class="">Task Price</div>
+              <div class="" v-if="isContractor">Task Price (Sub)</div>
+              <div class="" v-if="isContractor">Profit</div>
+            </div>
+            <div class="col-12" v-if="invoice.job_tasks.length > 0">
+              <div class="flex justify-between mb-2 mt-2" v-for="task in invoice.job_tasks" :key="task.id">
+                <div class="">{{ task.task.name }}</div>
+                <div class="">{{ task.qty }}</div>
+                <div class="">${{ task.cust_final_price }}</div>
+                <div class="" v-if="isContractor">${{ task.sub_final_price }}</div>
+                <div class="" v-if="isContractor">${{ task.cust_final_price -
                                 task.sub_final_price }}
-                            </div>
-                        </div>
-                        <div class="divider-line mt-2 mb-2"></div>
-                        <div class="flex justify-between">
-                            <div class="invoice-item">Totals:</div>
-                            <div class="invoice-item"></div>
-                            <div class="invoice-item">${{ totalCustomerPrice }}</div>
-                            <div class="invoice-item" v-if="isContractor">${{ totalSubPrice }}</div>
-                            <div class="invoice-item" v-if="isContractor">${{ totalCustomerPrice + totalSubPrice }}
-                            </div>
-                        </div>
-                    </div>
                 </div>
-            </card>
-        </div>
-        <feedback></feedback>
+              </div>
+              <div class="divider-line mt-2 mb-2"></div>
+              <div class="col-12">
+                <div class="">Totals:</div>
+                <div class=""></div>
+                <div class="">${{ totalCustomerPrice }}</div>
+                <div class="" v-if="isContractor">${{ totalSubPrice }}</div>
+                <div class="" v-if="isContractor">${{ totalCustomerPrice + totalSubPrice }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </card>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  import Feedback from '../components/shared/Feedback';
-  import Card from '../components/shared/Card';
 
   export default {
     props: {
       user: Object,
-    },
-    components: {
-      Card,
-      Feedback
     },
     data() {
       return {
@@ -132,7 +104,7 @@
 
 <style>
 
-    .invoice-item {
+    . {
         width: 40px;
     }
 
@@ -146,7 +118,7 @@
     }
 
     @media (min-width: 792px) {
-        .invoice-item {
+        . {
             width: 110px;
         }
     }
