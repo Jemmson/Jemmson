c48dbc1d0daa224ef927fffd07c18d26e0751349
Shawn Pike <pike.shawn@gmail.com>
2019-06-08 18:06:54 -0700

    updated the css on the registration page

--- resources/assets/js/pages/Register.vue
+++ resources/assets/js/pages/Register.vue
@@ -1,141 +1,153 @@
 <template>
 
-    <div class="row">
-        <div class="col-12">
-            <card>
-                <div class="jem-block">
-                    <h1>Who Are You?</h1>
-                    <div class="jem-buttons">
-                        <button
-                                class="btn btn-lg btn-primary flex-1 btleft"
-                                ref="customerButton"
-                                :class="userTypeSelected === 'customer' ? 'selected-button' : ''"
-                                v-on:click="userSelected('customer')">Customer
-                        </button>
-                        <button class="btn btn-lg btn-primary flex-1 btright"
-                                ref="contractorButton"
-                                :class="userTypeSelected === 'contractor' ? 'selected-button' : ''"
-                                v-on:click="userSelected('contractor')">Contractor
-                        </button>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <card>
+                    <h3 class="text-center">Who Are You?</h3>
+                    <div class="row">
+                        <div class="col-6">
+                            <button
+                                    class="btn btn-lg btn-primary jem-width"
+                                    ref="customerButton"
+                                    style="margin-left: -.65rem;"
+                                    :class="userTypeSelected === 'customer' ? 'selected-button' : ''"
+                                    v-on:click="userSelected('customer')">Customer
+                            </button>
+                        </div>
+                        <div class="col-6">
+                            <button class="btn btn-lg btn-primary jem-width"
+                                    ref="contractorButton"
+                                    :class="userTypeSelected === 'contractor' ? 'selected-button' : ''"
+                                    v-on:click="userSelected('contractor')">Contractor
+                            </button>
+                        </div>
                     </div>
-                </div>
 
-                <div class="jem-block jem-block-space" v-if="usesQuickbooks">
-                    <h3>Are You Using Quickbooks?</h3>
-                    <div class="jem-buttons">
-                        <a
-                                :href="quickbooks.auth_url"
-                                ref="quickbooks"
-                                class="flex-1 btleft"
-                        >
-                            <img
-                                    alt="qbo/docs/develop/authentication-and-authorization/C2QB_auth.png"
-                                    src="https://static.developer.intuit.com/images/C2QB_auth.png"
-                                    style="height: 100%; width: 100%"
+
+                    <h4 data-v-24a44010=""
+                        class="text-center"
+                        v-if="usesQuickbooks"
+                        style="margin-top: 1.5rem">Do You Use Quickbooks?</h4>
+
+                    <div class="row" v-if="usesQuickbooks">
+                        <div class="col-9">
+                            <a
+                                    :href="quickbooks.auth_url"
+                                    ref="quickbooks"
                             >
-                        </a>
+                                <img
+                                        alt="qbo/docs/develop/authentication-and-authorization/C2QB_auth.png"
+                                        src="https://static.developer.intuit.com/images/C2QB_auth.png"
+                                        style="height: 100%; width: 100%"
+                                        class="w-full"
+                                >
+                            </a>
+                        </div>
 
-                        <button class="btn btn-lg btn-primary flex-1 btright"
-                                v-on:click="doesNotUseQuickbooks()">None
+                        <button class="btn btn-lg btn-primary col-3"
+                                v-on:click="doesNotUseQuickbooks()"><span class="uppercase">no</span>
+                        </button>
+                    </div>
+
+                    <div v-if="showRegistration">
+
+                        <hr style="margin-top: 3rem">
+
+                        <div class="row">
+                            <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('first_name')">-->
+                            <label for="firstName" class="m-auto pt-1 pt-2" style="">First Name</label>
+                            <input
+                                    id="firstName"
+                                    type="text"
+                                    class="form-control m-auto"
+                                    @blur="checkValidData"
+                                    v-model="registerForm.first_name">
+                            <!--                <span class="help-block" v-show="registerForm.errors.has('first_name')"></span>-->
+                        </div>
+
+                        <div class="row">
+                            <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('last_name')">-->
+                            <label for="lastName" class="m-auto pt-3 pt-2">Last Name</label>
+                            <input id="lastName"
+                                   @blur="checkValidData"
+                                   type="text" class="form-control m-auto"
+                                   v-model="registerForm.last_name">
+                            <!--                <span class="help-block" v-show="registerForm.errors.has('last_name')"></span>-->
+                        </div>
+
+                        <div class="row">
+                            <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('email')">-->
+                            <label for="email" class="m-auto pt-3 pt-2">E-Mail Address</label>
+                            <input id="email"
+                                   @blur="checkValidData"
+                                   type="text" class="form-control m-auto"
+                                   v-model="registerForm.email">
+                            <!--                <span class="help-block" v-show="registerForm.errors.has('email')"></span>-->
+                        </div>
+
+                        <div class="row">
+                            <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('email')">-->
+                            <label for="password"
+                                   class="m-auto pt-3 pt-2">Password</label>
+                            <input id="password"
+                                   @blur="checkValidData"
+                                   type="password" class="form-control m-auto"
+                                   v-model="registerForm.password">
+                            <!--                <span class="help-block" v-show="registerForm.errors.has('email')"></span>-->
+                        </div>
+
+                        <div class="row">
+                            <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('email')">-->
+                            <label for="password_confirmation" class="m-auto pt-3 pt-2">Confirm
+                                Password</label>
+                            <input id="password_confirmation"
+                                   @blur="checkValidData"
+                                   type="password" class="form-control m-auto"
+                                   v-model="registerForm.password_confirmation">
+                            <!--                <span class="help-block" v-show="registerForm.errors.has('email')"></span>-->
+                        </div>
+
+
+                        <!-- Terms And Conditions -->
+
+                        <!--            <div class="input-section" :class="{'has-error': registerForm.errors.has('terms')}">-->
+
+                        <hr style="margin-top: 3rem">
+
+                        <div class="row pt-3 pt-2 m-auto">
+                            <input
+                                    type="checkbox"
+                                    @blur="checkValidData"
+                                    class="mr-2"
+                                    name="terms"
+                                    style="align-self: center;"
+                                    v-model="registerForm.terms">I Accept The
+                            <a href="/terms" target="_blank">Terms Of Service</a>
+                        </div>
+
+                        <button id="register" name=register
+                                class="register border shadow uppercase"
+                                @click.prevent="register"
+                                :disabled="registerForm.busy">
+                            <span v-if="registerForm.busy">
+                                <i class="fa fa-btn fa-spinner fa-spin"></i>Registering
+                            </span>
+                            <span v-else>
+                                <i class="fa fa-btn fa-check-circle mr-2"></i>Register
+                            </span>
                         </button>
 
-                    </div>
-                </div>
 
-                <div v-if="showRegistration">
-                    <div class="flex flex-col mt-2 mb-2">
-                        <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('first_name')">-->
-                        <label for="firstName" class="text-center" style="width: 95%;">First Name</label>
-                        <input
-                                id="firstName"
-                                type="text"
-                                class="form-control ml-auto mr-auto"
-                                style="width: 95%;"
-                                @blur="checkValidData"
-                                v-model="registerForm.first_name">
-                        <!--                <span class="help-block" v-show="registerForm.errors.has('first_name')"></span>-->
-                    </div>
+                        <div style="height: 10rem; width: 100%">
 
-                    <div class="flex flex-col mt-2 mb-2">
-                        <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('last_name')">-->
-                        <label for="lastName" class="text-center" style="width: 95%;">Last Name</label>
-                        <input id="lastName"
-                               @blur="checkValidData"
-                               type="text" class="form-control ml-auto mr-auto" style="width: 95%;"
-                               v-model="registerForm.last_name">
-                        <!--                <span class="help-block" v-show="registerForm.errors.has('last_name')"></span>-->
-                    </div>
+                        </div>
 
-                    <div class="flex flex-col mt-2 mb-2">
-                        <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('email')">-->
-                        <label for="email" class="text-center" style="width: 95%;">E-Mail Address</label>
-                        <input id="email"
-                               @blur="checkValidData"
-                               type="text" class="form-control ml-auto mr-auto" style="width: 95%;"
-                               v-model="registerForm.email">
-                        <!--                <span class="help-block" v-show="registerForm.errors.has('email')"></span>-->
-                    </div>
-
-                    <div class="flex flex-col mt-2 mb-2">
-                        <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('email')">-->
-                        <label for="password"
-                               class="text-center" style="width: 95%;">Password</label>
-                        <input id="password"
-                               @blur="checkValidData"
-                               type="password" class="form-control ml-auto mr-auto" style="width: 95%;"
-                               v-model="registerForm.password">
-                        <!--                <span class="help-block" v-show="registerForm.errors.has('email')"></span>-->
-                    </div>
-
-                    <div class="flex flex-col mt-2 mb-2">
-                        <!--            <div class="flex flex-col mt-2 mb-2" :class="{'has-error': registerForm.errors.has('email')">-->
-                        <label for="password_confirmation" class="text-center" style="width: 95%;">Confirm Password</label>
-                        <input id="password_confirmation"
-                               @blur="checkValidData"
-                               type="password" class="form-control ml-auto mr-auto"
-                               style="width: 95%;" v-model="registerForm.password_confirmation">
-                        <!--                <span class="help-block" v-show="registerForm.errors.has('email')"></span>-->
-                    </div>
-
-
-                    <!-- Terms And Conditions -->
-
-                    <!--            <div class="input-section" :class="{'has-error': registerForm.errors.has('terms')}">-->
-                    <div class="jem-buttons text-center justify-center align-center" style="width: 95%;">
-                        <input
-                                type="checkbox"
-                                @blur="checkValidData"
-                                name="terms"
-                                style="margin-right: .5rem"
-                                v-model="registerForm.terms">I Accept The
-                        <a href="/terms" target="_blank">Terms Of Service</a>
-                    </div>
-                    <!--                        <span class="help-block" v-show="registerForm.errors.has('terms')">-->
-                    <!--                                    @{{ registerForm.errors.get('terms') }}-->
-                    <!--                                </span>-->
-
-
-                    <button id="register" name=register
-                            class="register text-center border shadow uppercase btspace flex-1"
-                            @click.prevent="register"
-                            :disabled="registerForm.busy">
-                        <span v-if="registerForm.busy">
-                            <i class="fa fa-btn fa-spinner fa-spin"></i>Registering
-                        </span>
-                        <span v-else>
-                            <i class="fa fa-btn fa-check-circle mr-2"></i>Register
-                        </span>
-                    </button>
-
-
-                    <div style="height: 10rem; width: 100%">
 
                     </div>
 
-
-                </div>
-
-            </card>
+                </card>
+            </div>
         </div>
     </div>
 
@@ -203,7 +215,7 @@
         this.usesQuickbooks = false
         this.showRegistration = true
       },
-      checkValidData () {
+      checkValidData() {
         if (
           this.registerForm.first_name !== '' &&
           this.registerForm.last_name !== '' &&
@@ -256,51 +268,8 @@
 
 <style scoped>
 
-    .jem-block {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
-
-    .jem-block-space {
-        margin-top: 1rem;
-    }
-
-    .jem-buttons {
-        width: 100%;
-        display: flex;
-    }
-
-    .flex-1 {
-        width: 100%;
-    }
-
-    .btleft {
-        margin-left: 1rem;
-        margin-right: .5rem;
-    }
-
-    .btright {
-        margin-left: .5rem;
-        margin-right: 1rem;
-    }
-
-    .btspace {
-        margin-left: 1rem;
-        margin-right: 1rem;
-    }
-
-    .selected-button {
-        border: solid thick red;
-    }
-
-    .justify-center {
-        justify-content: center;
-    }
-
-    .align-center {
-        align-content: center;
+    .jem-width {
+        width: 106%
     }
 
     .register {
