b3e53631b934264a9e8ecb87105c128141d0c1b5
Shawn Pike <pike.shawn@gmail.com>
2019-06-26 16:16:26 -0600

    accidentally deleted the tests folder

---
+++ tests/Feature/LoggedInTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Contractor;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoggedInTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testLoggedInValue()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+
+        $contractor = factory(Contractor::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->actingAs($user)->json('GET', 'loggedIn');
+
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);
+    }
+}
