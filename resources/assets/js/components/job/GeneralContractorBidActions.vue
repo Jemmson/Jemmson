254f462d29fb971cf0b87eca12d11d6e3f694f23
Shawn Pike <pike.shawn@gmail.com>
2019-08-19 12:33:12 -0600

    reformatting

--- resources/assets/js/components/job/GeneralContractorBidActions.vue
+++ resources/assets/js/components/job/GeneralContractorBidActions.vue
@@ -10,58 +10,61 @@
 
         <button ref="submitBid"
                 class="btn btn-normal btn-sm"
-                @click="notifyCustomerOfFinishedBid()">Submit Bid</button>
-<!--                @click="openBidSubmissionDialog()">Submit Bid</button>-->
+                @click="notifyCustomerOfFinishedBid()">Submit Bid
+        </button>
+        <!--                @click="openBidSubmissionDialog()">Submit Bid</button>-->
 
     </div>
 
 </template>
 
 <script>
-    export default {
-      name: 'GeneralContractorBidActions',
-      props: {
-        submitTheBid: Boolean,
-        bid: Object
-      },
-      watch: {
-        submitTheBid: this.notifyCustomerOfFinishedBid
-      },
-      data(){
-        return {
-          subTaskWarning: false
-        }
-      },
-      methods: {
-        openBidSubmissionDialog() {
-          return this.$emit('open-bid-submission', true)
-        },
-        notifyCustomerOfFinishedBid(){
-          // go through each job task and compare the sub price to the contractor task price
-          // first check if there is a sub.
-          // check if the sub price is an accepted price
-          // compare the the accepted sub price to the contractor price
-          // if the accepted sub price is higher then throw an error
-
-
-          this.subTaskWarning = false
-          for (let i = 0; i < this.bid.job_tasks.length; i++) {
-            if (this.bid.job_tasks[i].sub_final_price > this.bid.job_tasks[i].cust_final_price) {
-              console.log('sub final price')
-              console.log(this.bid.job_tasks[i].sub_final_price)
-              console.log('cust final price')
-              console.log(this.bid.job_tasks[i].cust_final_price)
-              this.subTaskWarning = true
-            }
-          }
-
-          if (!this.subTaskWarning) {
-            GeneralContractor.notifyCustomerOfFinishedBid(this.bid, this.disabled)
-          }
-
+  export default {
+    name: 'GeneralContractorBidActions',
+    props: {
+      submitTheBid: Boolean,
+      bid: Object
+    },
+    watch: {
+      submitTheBid: this.notifyCustomerOfFinishedBid
+    },
+    data() {
+      return {
+        subTaskWarning: false,
+        disabled: {
+          submitBid: true
         }
       }
+    },
+    methods: {
+      openBidSubmissionDialog() {
+        return this.$emit('open-bid-submission', true)
+      },
+      notifyCustomerOfFinishedBid() {
+        // go through each job task and compare the sub price to the contractor task price
+        // first check if there is a sub.
+        // check if the sub price is an accepted price
+        // compare the the accepted sub price to the contractor price
+        // if the accepted sub price is higher then throw an error
+
+        this.subTaskWarning = false
+        for (let i = 0; i < this.bid.job_tasks.length; i++) {
+          if (this.bid.job_tasks[i].sub_final_price > this.bid.job_tasks[i].cust_final_price) {
+            console.log('sub final price')
+            console.log(this.bid.job_tasks[i].sub_final_price)
+            console.log('cust final price')
+            console.log(this.bid.job_tasks[i].cust_final_price)
+            this.subTaskWarning = true
+          }
+        }
+
+        if (!this.subTaskWarning) {
+          GeneralContractor.notifyCustomerOfFinishedBid(this.bid, this.disabled)
+        }
+
+      }
     }
+  }
 </script>
 
 <style>
