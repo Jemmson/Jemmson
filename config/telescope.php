1af47a6b5a4d3ba967422b00d0b4c6382618c7bc
Shawn Pike <pike.shawn@gmail.com>
2019-09-18 13:58:46 -0700

    commented out watchers in the telescope file so that the phpunit tests will work

--- config/telescope.php
+++ config/telescope.php
@@ -111,7 +111,7 @@ return [
     */
 
     'watchers' => [
-        Watchers\CacheWatcher::class => env('TELESCOPE_CACHE_WATCHER', true),
+//        Watchers\CacheWatcher::class => env('TELESCOPE_CACHE_WATCHER', true),
 
 //        Watchers\CommandWatcher::class => [
 //            'enabled' => env('TELESCOPE_COMMAND_WATCHER', true),
@@ -119,11 +119,11 @@ return [
 //        ],
 
 //        Watchers\DumpWatcher::class => env('TELESCOPE_DUMP_WATCHER', true),
-        Watchers\EventWatcher::class => env('TELESCOPE_EVENT_WATCHER', true),
-        Watchers\ExceptionWatcher::class => env('TELESCOPE_EXCEPTION_WATCHER', true),
-        Watchers\JobWatcher::class => env('TELESCOPE_JOB_WATCHER', true),
+//        Watchers\EventWatcher::class => env('TELESCOPE_EVENT_WATCHER', true),
+//        Watchers\ExceptionWatcher::class => env('TELESCOPE_EXCEPTION_WATCHER', true),
+//        Watchers\JobWatcher::class => env('TELESCOPE_JOB_WATCHER', true),
 //        Watchers\LogWatcher::class => env('TELESCOPE_LOG_WATCHER', true),
-        Watchers\MailWatcher::class => env('TELESCOPE_MAIL_WATCHER', true),
+//        Watchers\MailWatcher::class => env('TELESCOPE_MAIL_WATCHER', true),
 
 //        Watchers\ModelWatcher::class => [
 //            'enabled' => env('TELESCOPE_MODEL_WATCHER', true),
@@ -138,19 +138,19 @@ return [
 //            'slow' => 100,
 //        ],
 
-        Watchers\RedisWatcher::class => env('TELESCOPE_REDIS_WATCHER', true),
+//        Watchers\RedisWatcher::class => env('TELESCOPE_REDIS_WATCHER', true),
 
-        Watchers\RequestWatcher::class => [
-            'enabled' => env('TELESCOPE_REQUEST_WATCHER', true),
-            'size_limit' => env('TELESCOPE_RESPONSE_SIZE_LIMIT', 64),
-        ],
+//        Watchers\RequestWatcher::class => [
+//            'enabled' => env('TELESCOPE_REQUEST_WATCHER', true),
+//            'size_limit' => env('TELESCOPE_RESPONSE_SIZE_LIMIT', 64),
+//        ],
 
-        Watchers\GateWatcher::class => [
-            'enabled' => env('TELESCOPE_GATE_WATCHER', true),
-            'ignore_abilities' => [],
-            'ignore_packages' => true,
-        ],
-
-        Watchers\ScheduleWatcher::class => env('TELESCOPE_SCHEDULE_WATCHER', true),
+//        Watchers\GateWatcher::class => [
+//            'enabled' => env('TELESCOPE_GATE_WATCHER', true),
+//            'ignore_abilities' => [],
+//            'ignore_packages' => true,
+//        ],
+//
+//        Watchers\ScheduleWatcher::class => env('TELESCOPE_SCHEDULE_WATCHER', true),
     ],
 ];
