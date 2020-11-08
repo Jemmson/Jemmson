fc14b86d448953d57b8a7f96a1a3aa002aae7376
Shawn Pike <pike.shawn@gmail.com>
2020-11-08 14:06:36 -0700

    adding subs work

---
+++ resources/assets/js/components/documentation/components/DocumentationBase.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card-texttext>
+    <v-card-subtitle></v-card-subtitle>
+  </v-card-texttext>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
