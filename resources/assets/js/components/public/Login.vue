67aacf0d3762bc379746c16572904a5fe6ebbbea
Shawn Pike <pike.shawn@gmail.com>
2021-08-15 14:51:10 -0700

    cleaned up the login page

--- resources/assets/js/components/public/Login.vue
+++ resources/assets/js/components/public/Login.vue
@@ -1,103 +1,107 @@
 <template>
-    <v-card width="100%"
+  <v-card width="1300"
+          style="width: 750px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 4rem;"
+  >
+    <v-card-title class="w-break pb-0 flex flex-col">
+      <template
+          style="font-size: 1.5rem;"
+      >Login Or
+        <v-btn
+            id="register"
+            to="/register"
+            width="40%"
+            text
+            class="capitalize"
+            color="success">
+          Click here to Register
+        </v-btn>
+      </template>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+            id="username"
+            data-cy="username"
+            label="Email"
+            v-model="form.username"
+            prepend-icon="mdi-account-circle"
+        />
+        <v-text-field
+            id="password"
+            data-cy="password"
+            :type="showPassword ? 'text' : 'password'"
+            v-model="form.password"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions
+      style="padding: 0px 0px 17px 47px;"
     >
-        <v-card-title class="w-break pb-0 flex flex-col">
-            <template
-                    style="font-size: 1.5rem;"
-            >Login Or
-                <v-btn
-                        id="register"
-                        to="/register"
-                        width="40%"
-                        text
-                        class="capitalize"
-                        color="success">
-                    Click here to Register
-                </v-btn>
-            </template>
-        </v-card-title>
-        <v-card-text>
-            <v-form>
-                <v-text-field
-                        id="username"
-                        data-cy="username"
-                        label="Email"
-                        v-model="form.username"
-                        prepend-icon="mdi-account-circle"
-                />
-                <v-text-field
-                        id="password"
-                        data-cy="password"
-                        :type="showPassword ? 'text' : 'password'"
-                        v-model="form.password"
-                        label="Password"
-                        prepend-icon="mdi-lock"
-                        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                        @click:append="showPassword = !showPassword"
-                />
-            </v-form>
-        </v-card-text>
-        <v-card-actions>
-            <div></div>
-            <v-spacer></v-spacer>
-            <v-btn
-                    id="submit"
-                    data-cy="submit"
-                    color="info"
-                    :loading="form.busy"
-                    width="40%"
-                    @click="logIntoApp(form)"
-            >Login
-            </v-btn>
-        </v-card-actions>
-        <v-divider></v-divider>
-        <v-col cols="12" class="text-center">
-            <v-row justify="center" align-content="center">
-                <v-checkbox
-                        v-model="form.remember"
-                        class=""
-                        label="Remember Me"></v-checkbox>
-            </v-row>
+      <v-btn
+          id="submit"
+          data-cy="submit"
+          color="info"
+          :loading="form.busy"
+          width="40%"
+          @click="logIntoApp(form)"
+      >Login
+      </v-btn>
+    </v-card-actions>
+    <v-divider></v-divider>
+    <v-col cols="12" class="text-center">
+      <v-row justify="center" align-content="center">
+        <v-checkbox
+            v-model="form.remember"
+            class=""
+            label="Remember Me"></v-checkbox>
+      </v-row>
 
-            <v-btn
-                    text
-                    color="primary"
-                    link
-                    @click="$router.push('/passwordEmailVerification')"
-            >Forgot Your Password?
-            </v-btn>
-        </v-col>
+      <v-btn
+          text
+          color="primary"
+          link
+          @click="$router.push('/passwordEmailVerification')"
+      >Forgot Your Password?
+      </v-btn>
+    </v-col>
 
-    </v-card>
+  </v-card>
 </template>
 
 <script>
-    import {mapActions} from 'vuex'
+import {mapActions} from 'vuex'
 
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                showLogin: true,
-                showPassword: false,
-                form: {
-                    username: '',
-                    password: '',
-                    remember: null,
-                    error: null,
-                    busy: false
-                },
-            }
-        },
-        methods: {
-            ...mapActions([
-                'login'
-            ]),
-            logIntoApp(form) {
-                this.login(form)
-            },
-        }
+export default {
+  name: 'Login',
+  data() {
+    return {
+      showLogin: true,
+      showPassword: false,
+      form: {
+        username: '',
+        password: '',
+        remember: null,
+        error: null,
+        busy: false
+      },
     }
+  },
+  methods: {
+    ...mapActions([
+      'login'
+    ]),
+    logIntoApp(form) {
+      this.login(form)
+    },
+  }
+}
 </script>
 
 <style>
